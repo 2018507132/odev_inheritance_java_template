fa136e5306752e9f4a854294a6ef511544d9de08
2018507132 <92215358+2018507132@users.noreply.github.com>
2021-11-17 20:51:33 +0300

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,12 +1,12 @@
 public class Circle extends Ellipse
 {
 	//icerisine r yi parametre olarak alan bir constructor tanimla
-   constructor
-   {
-	   //atasinin constructorunu calistir
-      super(r, r); 
-	  //shapeName icerisine "Circle" yaz
-	  this.shapeName = "Circle";
-   }
+  public String shapeName="Circle";
+	public void Circle(int r){ 
+		super(r1,r2);
+		r1=r;
+		r2=r;
+		
+	}
    
 }
