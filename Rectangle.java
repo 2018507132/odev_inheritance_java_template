40db17e41ac024d75fdf6eebcd181429fa843d29
2018507132 <92215358+2018507132@users.noreply.github.com>
2021-11-17 20:59:24 +0300

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,22 +1,17 @@
 public class Rectangle extends Shape
-{
-	// dikdortgenin iki kenarini tutmak icin private degiskenler tanimla
-   
-   
-	//iki kenar uzunlugunu parametre olarak alan bir constructor tanimla
-   constructor
-   {
-	   //kenar uzunluklari atamalarini gerceklestir
-
-
-	  //shapeName icerisine "Rectangle" yaz
-
-
-   }
-	//iki kenar bilgisi ile alan hesaplayip return eden getArea methodunu tanimla
-   
-   getArea
-   {
-      
+{public int width;
+	public int heigth;
+	public String shapeName= "Rectangle";
+	public void Rectangle(int pwidth,int pheigth){
+		
+		width=pwidth;
+		heigth=pheigth;
+		
+	}
+	
+	public int getArea(){
+		int result= width * heigth;
+		return result;
+	}
    }
 }
