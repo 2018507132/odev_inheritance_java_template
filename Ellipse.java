f2f50ab6345f48fdddce6d5e98e23893cd12e920
2018507132 <92215358+2018507132@users.noreply.github.com>
2021-11-17 20:56:48 +0300

    Update Ellipse.java

--- Ellipse.java
+++ Ellipse.java
@@ -1,26 +1,17 @@
 public class Ellipse extends Shape
 {
-	//final PI degiskeni 3 olacak sekilde tanimla
-
-
-   //r1 ve r2 yi tanimla
-
-
-
-	//r1 r2 yi parametre olarak alan bir constructor tanimla
-   constructor
-   {
-	   //r1 ve r2 atamalarini yap
-
-
-	  // shapeName icerisine "Ellipse" yaz
-	  this.shapeName = "Ellipse";
-   }
-
-	//PI * r1 * r2 sonucunu donen bir getArea fonksiyonu tanimla
+   	public String shapeName= "Triangle";	
+	public void Triangle(int pwidth, int pheigth){
+		
+		width=pwidth;
+		heigth=pheigth;
+		
+		 
+	}	
+	
+	public int getArea(){
+		int result= width * heigth / 2;
+		return result;
+	}   
    
-   getArea
-   {
-      
-   }
 }
